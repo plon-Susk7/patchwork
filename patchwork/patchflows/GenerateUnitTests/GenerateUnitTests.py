d4334a5de7703a59a26938fca980ab7a82468f67
priyash7 <priyashshah4@gmail.com>
2024-11-01 17:24:59 +0530

    minor changes

--- patchwork/patchflows/GenerateUnitTests/GenerateUnitTests.py
+++ patchwork/patchflows/GenerateUnitTests/GenerateUnitTests.py
@@ -1,35 +1,53 @@
 import json
 from pathlib import Path
-
 import yaml
 
+from patchwork.common.utils.step_typing import validate_steps_with_inputs
 from patchwork.step import Step
 from patchwork.steps import (
-    LLM,
+    LLM, 
     ReadFile,
 )
 
 _DEFAULT_INPUT_FILE = Path(__file__).parent / "defaults.yml"
 _DEFAULT_PROMPT_JSON = Path(__file__).parent / "prompt.json"
 
-# Allow users to specify classes to test
-# Automatically generate unit tests for methods in those classes
-# Decide on what framework we will use for generating tests
-
-# We'll first use pytest for this purpose
-
-
 class GenerateUnitTests(Step):
-
     def __init__(self, inputs):
         super().__init__(inputs)
 
         final_inputs = yaml.safe_load(_DEFAULT_INPUT_FILE.read_text())
-        self_inputs = final_inputs
+        if final_inputs is None:
+            final_inputs = {}
+        
+        final_inputs.update(inputs)
+
+        # if "prompt_id" not in final_inputs.keys():
+        #     final_inputs["prompt_id"] = "fixprompt"
+
+        final_inputs["prompt_id"] = "GenerateUnitTests"
+        if "prompt_template_file" not in final_inputs:
+            final_inputs["prompt_template_file"] = _DEFAULT_PROMPT_JSON
+
+        validate_steps_with_inputs(
+            set(final_inputs.keys()).union({"prompt_values"}), LLM, ReadFile
+        )
         print(final_inputs)
+        self.inputs = final_inputs
 
     def run(self):
-        with open(self.file, "r") as f:
-            file_contents = f.read()
+        output =ReadFile(self.inputs).run()
+        self.inputs.update(output)
+        self.inputs["prompt_values"] = [output]
+        outputs = LLM(self.inputs).run()
+        print(outputs)
+        # print(output)
 
-        return dict(file_path=self.file, file_content=file_contents)
+
+# Assuming you have a valid SARIF file called 'example.sarif' in the same directory
+# input = {
+#     "path": "/home/priyash7/Desktop/open-source/patchwork/patchwork/patchflows/GenerateUnitTests/GenerateUnitTests.py",  # Path to your SARIF file
+# }
+
+# generate_unit_tests = GenerateUnitTests(input)
+# generate_unit_tests.run()
